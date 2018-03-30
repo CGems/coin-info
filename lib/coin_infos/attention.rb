185ca5951bf3317457669e12b361b00ca4ec7986
Leon Oldfritter <leon.zcf@gmail.com>
2018-03-30 13:28:46 +0800

    0.0.04

--- lib/coin_infos/attention.rb
+++ lib/coin_infos/attention.rb
@@ -1,45 +1,43 @@
-module CoinInfos
-  class Attention
+class CoinInfos::Attention
   
-    class << self
+  class << self
     
-      def name
-        'atm'
-      end
-
-      def key
-        'attention'
-      end
-
-      def website
-        ''
-      end
-
-      def coinmarketcap_url
-        ''
-      end
-
-      def contact_address
-        ''
-      end
-
-      def twitter
-        ''
-      end
-
-      def facebook
-        ''
-      end
-
-      def github
-        ''
-      end
-
-      def logo_url
-        'https://rfinex.com/icon-atm.png'
-      end
-
+    def name
+      'atm'
     end
-  
+
+    def key
+      'attention'
+    end
+
+    def website
+      ''
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      ''
+    end
+
+    def twitter
+      ''
+    end
+
+    def facebook
+      ''
+    end
+
+    def github
+      ''
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-atm.png'
+    end
+
   end
+  
 end
--- lib/coin_infos/cetoken.rb
+++ lib/coin_infos/cetoken.rb
@@ -1,45 +1,43 @@
-module CoinInfos
-  class Cetoken
+class CoinInfos::Cetoken
   
-    class << self
+  class << self
     
-      def name
-        'cxtc'
-      end
-
-      def key
-        'cetoken'
-      end
-
-      def website
-        ''
-      end
-
-      def coinmarketcap_url
-        ''
-      end
-
-      def contact_address
-        ''
-      end
-
-      def twitter
-        ''
-      end
-
-      def facebook
-        ''
-      end
-
-      def github
-        ''
-      end
-
-      def logo_url
-        'https://rfinex.com/icon-cxtc.png'
-      end
-
+    def name
+      'cxtc'
     end
-  
+
+    def key
+      'cetoken'
+    end
+
+    def website
+      ''
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      ''
+    end
+
+    def twitter
+      ''
+    end
+
+    def facebook
+      ''
+    end
+
+    def github
+      ''
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-cxtc.png'
+    end
+
   end
+  
 end
