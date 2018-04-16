8254af9d712573d505bb26c5e5f2ee817d2a3eb5
Leon Oldfritter <leon.zcf@gmail.com>
2018-04-16 09:30:35 +0800

    add gst

---
+++ lib/coin_infos/ginsentoken.rb
@@ -0,0 +1,43 @@
+class CoinInfos::Ginsentoken
+  
+  class << self
+    
+    def name
+      'gst'
+    end
+
+    def key
+      'ginsentoken'
+    end
+
+    def website
+      'http://www.ginsengchain.org'
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      ''
+    end
+
+    def twitter
+      'https://twitter.com/ChainGinseng'
+    end
+
+    def facebook
+      'https://www.facebook.com/GinsengChain.org'
+    end
+
+    def github
+      ''
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-gst.png'
+    end
+
+  end
+  
+end
