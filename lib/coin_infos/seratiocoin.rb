453de9fa97cff2122295060216d7ed59c666133c
Leon Oldfritter <leon.zcf@gmail.com>
2018-04-23 12:16:12 +0800

    add seratiocoin

---
+++ lib/coin_infos/seratiocoin.rb
@@ -0,0 +1,43 @@
+class CoinInfos::Seratiocoin
+  
+  class << self
+    
+    def name
+      'ser'
+    end
+
+    def key
+      'seratiocoin'
+    end
+
+    def website
+      'https://www.seratio-coins.world'
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      'https://etherscan.io/token/0xB561fEF0d624C0826ff869946f6076B7c4f2ba42'
+    end
+
+    def twitter
+      'https://twitter.com/theSERatio'
+    end
+
+    def facebook
+      'https://www.facebook.com/TheCCEG/'
+    end
+
+    def github
+      'https://github.com/seratio/whitepaper'
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-ser.png'
+    end
+
+  end
+  
+end
