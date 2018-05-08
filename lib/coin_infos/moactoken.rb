dcd051470dc513d345f23e8c11fffe762c590ac9
Leon Oldfritter <leon.zcf@gmail.com>
2018-05-08 17:04:01 +0800

    add moactoken

---
+++ lib/coin_infos/moactoken.rb
@@ -0,0 +1,43 @@
+class CoinInfos::Moactoken
+  
+  class << self
+    
+    def name
+      'moc'
+    end
+
+    def key
+      'moactoken'
+    end
+
+    def website
+      ''
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      ''
+    end
+
+    def twitter
+      ''
+    end
+
+    def facebook
+      ''
+    end
+
+    def github
+      ''
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-moc.png'
+    end
+
+  end
+  
+end
