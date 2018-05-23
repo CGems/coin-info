275edca6a714c0057fb991fe801a17c036dadcb9
Leon Oldfritter <leon.zcf@gmail.com>
2018-05-23 11:06:18 +0800

    add GSC

---
+++ lib/coin_infos/globalsocialchain.rb
@@ -0,0 +1,45 @@
+module CoinInfos
+  class Globalsocialchain
+  
+    class << self
+    
+      def name
+        'gsc'
+      end
+
+      def key
+        'globalsocialchain'
+      end
+
+      def website
+        'http://www.gsc.social'
+      end
+
+      def coinmarketcap_url
+        ''
+      end
+
+      def contact_address
+        'https://etherscan.io/address/0x228ba514309ffdf03a81a205a6d040e429d6e80c'
+      end
+
+      def twitter
+        'https://twitter.com/gsc_socialchain'
+      end
+
+      def facebook
+        ''
+      end
+
+      def github
+        ''
+      end
+
+      def logo_url
+        'https://i.loli.net/2018/05/16/5afbaea796cf5.png'
+      end
+
+    end
+  
+  end
+end
