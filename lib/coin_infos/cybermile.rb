5648847557b3e61d3be9daea62629f35b458d8b3
Leon Oldfritter <leon.zcf@gmail.com>
2018-05-29 20:19:48 +0800

    add cmt

---
+++ lib/coin_infos/cybermile.rb
@@ -0,0 +1,45 @@
+module CoinInfos
+  class Cybermile
+  
+    class << self
+    
+      def name
+        'cmt'
+      end
+
+      def key
+        'cybermile'
+      end
+
+      def website
+        'https://www.cybermiles.io/'
+      end
+
+      def coinmarketcap_url
+        'https://coinmarketcap.com/currencies/cybermiles/'
+      end
+
+      def contact_address
+        'https://etherscan.io/address/0xf85feea2fdd81d51177f6b8f35f0e6734ce45f5f'
+      end
+
+      def twitter
+        'https://twitter.com/cybermiles'
+      end
+
+      def facebook
+        ''
+      end
+
+      def github
+        'https://github.com/cybermiles'
+      end
+
+      def logo_url
+        'https://avatars3.githubusercontent.com/u/32409086?s=200&v=4'
+      end
+
+    end
+  
+  end
+end
