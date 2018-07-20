26fd507089608da480ddd58a62d97460a53238dc
Leon Oldfritter <leon.zcf@gmail.com>
2018-07-20 16:05:06 +0800

    add maximinecoin

---
+++ lib/coin_infos/maximinecoin.rb
@@ -0,0 +1,43 @@
+class CoinInfos::Maximinecoin
+  
+  class << self
+    
+    def name
+      'mxm'
+    end
+
+    def key
+      'maximinecoin'
+    end
+
+    def website
+      ''
+    end
+
+    def coinmarketcap_url
+      ''
+    end
+
+    def contact_address
+      'https://etherscan.io/address/0x6a750d255416483bec1a31ca7050c6dac4263b57'
+    end
+
+    def twitter
+      'https://twitter.com/maximinecoin'
+    end
+
+    def facebook
+      ''
+    end
+
+    def github
+      ''
+    end
+
+    def logo_url
+      'https://rfinex.com/icon-mxm.png'
+    end
+
+  end
+  
+end
